70db7e62858a33f45a03f15ac2552d1daf5cbb30
Matthew Carter <m.j.carter2@liverpool.ac.uk>
2023-06-23 17:29:49 +0100

    feat: update example to sample multiple chains

--- examples/multivariate_normal.py
+++ examples/multivariate_normal.py
@@ -26,10 +26,10 @@ class Target():
 
 
 def main():
-    K = 100
+    n_chains = 4
+    num_warmup = 500
+    num_samples = 500
     dim = 5
-    num_steps = 5
-    step_size = 0.1
 
     target = Target(
         dim=dim,
@@ -38,34 +38,65 @@ def main():
     )
 
     sample_proposal = multivariate_normal(mean=np.zeros(target.dim), cov=np.eye(target.dim))
+
+    # Initialise the RW proposal
+    forward_kernel = RandomWalkProposal(
+        target=target,
+    )
+
+    # Run the RW sampler for each chain
+    rw_mcmc_chains = []
+    for i in range(n_chains):
+        rw_mcmc_chains.append(
+            MCMCSampler(
+                num_warmup=num_warmup,
+                num_samples=num_samples,
+                target=target,
+                forward_kernel=forward_kernel,
+                sample_proposal=sample_proposal,
+            )
+        )
+
+        rw_mcmc_chains[i].sample()
+
+    # Filter out the warmup samples, concatenate the chains
+    rw_mcmc_samples = np.concatenate([chain.x[num_warmup:] for chain in rw_mcmc_chains], axis=0)
+
+    print(f"RW Mean estimate:\n{np.mean(rw_mcmc_samples, axis=0)}")
+    print(f"RW Variance estimate:\n{np.var(rw_mcmc_samples, axis=0)}")
+
+    # Initialise the HMC proposal
+    num_steps = 5
+    step_size = 0.1
     momentum_proposal = multivariate_normal(mean=np.zeros(target.dim), cov=np.eye(target.dim))
     integrator = LeapfrogIntegrator(target=target, step_size=step_size)
-
     forward_kernel = HMCProposal(
-        dim=target.dim,
         target=target,
         num_steps=num_steps,
         momentum_proposal=momentum_proposal,
         integrator=integrator,
     )
 
-    # forward_kernel = RandomWalkProposal(
-    #     target=target,
-    #     dim=target.dim,
-    # )
+    # Run the HMC sampler for each chain
+    hmc_mcmc_chains = []
+    for i in range(n_chains):
+        hmc_mcmc_chains.append(
+            MCMCSampler(
+                num_warmup=num_warmup,
+                num_samples=num_samples,
+                target=target,
+                forward_kernel=forward_kernel,
+                sample_proposal=sample_proposal,
+            )
+        )
 
-    rw_mcmc = MCMCSampler(
-        K=K,
-        dim=target.dim,
-        target=target,
-        forward_kernel=forward_kernel,
-        sample_proposal=sample_proposal,
-    )
+        hmc_mcmc_chains[i].sample()
 
-    rw_mcmc.sample()
+    # Filter out the warmup samples, concatenate the chains
+    hmc_mcmc_samples = np.concatenate([chain.x[num_warmup:] for chain in hmc_mcmc_chains], axis=0)
 
-    print(f"Mean estimate:\n{np.mean(rw_mcmc.x, axis=0)}")
-    print(f"Variance estimate:\n{np.var(rw_mcmc.x, axis=0)}")
+    print(f"HMC Mean estimate:\n{np.mean(hmc_mcmc_samples, axis=0)}")
+    print(f"HMC Variance estimate:\n{np.var(hmc_mcmc_samples, axis=0)}")
 
 
 if __name__ == "__main__":
